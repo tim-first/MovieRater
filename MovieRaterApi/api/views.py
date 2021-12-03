c1b2f59fbfaaf0c7db8596e8c4ea77ab9ccb04a3
Name <you@example.com>
2021-12-03 02:04:38 -0800

    API part 8

--- MovieRaterApi/api/views.py
+++ MovieRaterApi/api/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import User
+
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
@@ -13,12 +15,23 @@ class MovieViewSet(viewsets.ModelViewSet):
 	@action(detail=True, methods=['POST'])
 	def rate_movie(self, request, pk=None):
 		if 'stars' in request.data:
-
 			movie = Movie.objects.get(id=pk)
-			print('Movie title: ', movie.title)
-
-			response = {'message': 'its working'}
-			return Response(response, status=status.HTTP_200_OK)
+			stars = request.data['stars']
+			# user = request.user
+			user = User.objects.get(id=1)
+			print('User: ', user.username)
+			try:
+				rating = Rating.objects.get(user=user.id, movie=movie.id)
+				rating.stars = stars
+				rating.save()
+				serializer = RatingSerializer(rating, many=False)
+				response = {'message': 'Rating updated', 'result': serializer.data}
+				return Response(response, status=status.HTTP_200_OK)
+			except:
+				rating = Rating.objects.create(user=user, movie=movie, stars=stars)
+				serializer = RatingSerializer(rating, many=False)
+				response = {'message': 'Rating created', 'result': serializer.data}
+				return Response(response, status=status.HTTP_200_OK)
 		else:
 			response = {'message': 'You need to provide stars'}
 			return Response(response, status=status.HTTP_400_BAD_REQUEST)
