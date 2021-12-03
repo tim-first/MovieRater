75a9b083b423222ac0a438730b12fd4e694d883a
Name <you@example.com>
2021-12-03 01:44:29 -0800

    API part 7

--- MovieRaterApi/api/views.py
+++ MovieRaterApi/api/views.py
@@ -12,8 +12,16 @@ class MovieViewSet(viewsets.ModelViewSet):
 
 	@action(detail=True, methods=['POST'])
 	def rate_movie(self, request, pk=None):
-		response = {'message': 'its working'}
-		return Response(response, status=status.HTTP_200_OK)
+		if 'stars' in request.data:
+
+			movie = Movie.objects.get(id=pk)
+			print('Movie title: ', movie.title)
+
+			response = {'message': 'its working'}
+			return Response(response, status=status.HTTP_200_OK)
+		else:
+			response = {'message': 'You need to provide stars'}
+			return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 
 class RatingViewSet(viewsets.ModelViewSet):
