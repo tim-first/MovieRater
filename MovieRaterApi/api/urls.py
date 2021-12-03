65effee165b18d1ad986fec1e018f6215c97e102
Name <you@example.com>
2021-12-03 00:49:25 -0800

    API part 4

--- MovieRaterApi/api/urls.py
+++ MovieRaterApi/api/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from django.urls import include
 from rest_framework import routers
 
+from .views import MovieViewSet, RatingViewSet
+
+
 router = routers.DefaultRouter()
+router.register('movies', MovieViewSet)
+router.register('ratings', RatingViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
