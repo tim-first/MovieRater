03112b7fffb93555ce0371660e1866a4b429e157
Name <you@example.com>
2021-12-03 04:41:05 -0800

    API part 12

--- MovieRaterApi/api/serializers.py
+++ MovieRaterApi/api/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
 from .models import Movie, Rating
 
@@ -13,6 +14,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 	def create(self, validated_data):
 		user = User.objects.create_user(**validated_data)
+		Token.objects.create(user=user)
 		return user
 
 
