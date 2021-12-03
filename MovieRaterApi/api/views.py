ba39d0a7c5242b1b399a73961ad9e5ae96fc2278
Name <you@example.com>
2021-12-03 01:36:45 -0800

    API part 6

--- MovieRaterApi/api/views.py
+++ MovieRaterApi/api/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer
@@ -7,6 +9,11 @@ from .serializers import MovieSerializer, RatingSerializer
 class MovieViewSet(viewsets.ModelViewSet):
 	queryset = Movie.objects.all()
 	serializer_class = MovieSerializer
+
+	@action(detail=True, methods=['POST'])
+	def rate_movie(self, request, pk=None):
+		response = {'message': 'its working'}
+		return Response(response, status=status.HTTP_200_OK)
 
 
 class RatingViewSet(viewsets.ModelViewSet):
