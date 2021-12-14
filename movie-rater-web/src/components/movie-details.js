a6af1b2f253ed9def4c2e3ed90a626531ad78612
Name <you@example.com>
2021-12-14 03:54:08 -0800

    frontend p7. rating

--- movie-rater-web/src/components/movie-details.js
+++ movie-rater-web/src/components/movie-details.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 
+var FontAwesome = require('react-fontawesome');
+
 class MovieDetails extends Component {
 
 	render() {
+		const mov = this.props.movie;
+
 		return (
 			<React.Fragment>
-				{ this.props.movie ? (
+				{ mov ? (
 					<div>
-						<h3>{this.props.movie.title}</h3>
-						<p>{this.props.movie.description}</p>
+						<h3>{mov.title}</h3>
+						<FontAwesome name="star" className={mov.avg_rating > 0 ? 'orange' : ''}/>
+						<FontAwesome name="star" className={mov.avg_rating > 1 ? 'orange' : ''}/>
+						<FontAwesome name="star" className={mov.avg_rating > 2 ? 'orange' : ''}/>
+						<FontAwesome name="star" className={mov.avg_rating > 3 ? 'orange' : ''}/>
+						<FontAwesome name="star" className={mov.avg_rating > 4 ? 'orange' : ''}/>
+						({mov.no_of_ratings})
+						<p>{mov.description}</p>
 					</div>
 				) : null }
 			</React.Fragment>
