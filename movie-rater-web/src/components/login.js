93fa9871c0dd655df3d917b08ca54e7c61159017
Name <you@example.com>
2021-12-21 03:39:23 -0800

    frontend p18 register

--- movie-rater-web/src/components/login.js
+++ movie-rater-web/src/components/login.js
@@ -8,7 +8,8 @@ class Login extends Component {
     credentials: {
       username: '',
       password: ''
-    }
+    },
+    isLoginView: true
   }
 
   inputChanged = event => {
@@ -17,29 +18,51 @@ class Login extends Component {
     this.setState({credentials: cred});
   }
   login = event => {
-    fetch(`${process.env.REACT_APP_API_URL}/auth/`, {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(this.state.credentials)
-    }).then( resp => resp.json())
-    .then( res => {
-      console.log(res.token);
-      this.props.cookies.set('mr-token', res.token);
-      window.location.href = "/movies";
-    })
-    .catch( error => console.log(error))
+    if(this.state.isLoginView) {
+      fetch(`${process.env.REACT_APP_API_URL}/auth/`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(this.state.credentials)
+      }).then( resp => resp.json())
+      .then( res => {
+        console.log(res.token);
+        this.props.cookies.set('mr-token', res.token);
+        window.location.href = "/movies";
+      })
+      .catch( error => console.log(error))
+    } else {
+      fetch(`${process.env.REACT_APP_API_URL}/api/users/`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(this.state.credentials)
+      }).then( resp => resp.json())
+      .then( res => {
+        this.setState({isLoginView: true});
+      })
+      .catch( error => console.log(error))
+    }
+  }
+  toggleView = () => {
+    this.setState({isLoginView: !this.state.isLoginView});
   }
 
   render(){
     return <div className="login-container">
-      <h1>Login</h1>
+      <h1>
+        { this.state.isLoginView ? 'Login' : 'Register' }
+      </h1>
       <span>Username</span><br />
       <input type="text" name="username" value={this.state.credentials.username}
         onChange={this.inputChanged} /><br />
       <span>Password</span><br />
       <input type="password" name="password" value={this.state.credentials.password}
         onChange={this.inputChanged} /><br />
-      <button onClick={this.login}>Login</button>
+      <button onClick={this.login}>
+        { this.state.isLoginView ? 'Login' : 'Register' }
+      </button>
+      <p onClick={this.toggleView}>
+        { this.state.isLoginView ? 'Create Account' : 'Back to Login' }
+      </p>
     </div>
   }
 }
