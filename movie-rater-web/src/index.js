0e9885c405c9a9ae574e5d2aa0eb0445d5d4e763
Name <you@example.com>
2021-12-21 02:21:00 -0800

    frontend p15 routing

--- movie-rater-web/src/index.js
+++ movie-rater-web/src/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Login from './components/login';
 import reportWebVitals from './reportWebVitals';
+import { render } from "react-dom";
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="movies" element={<App />} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
