74c4d6697e5d7ba86369c2f7dc0da93f0325355b
Name <you@example.com>
2022-01-15 21:24:30 -0800

    mobile p6 icons of rating

--- MovieRaterMobile/components/detail.js
+++ MovieRaterMobile/components/detail.js
@@ -1,4 +1,6 @@
 import { StyleSheet, Text, View} from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 export default function Detail(props) {
 
@@ -6,7 +8,16 @@ export default function Detail(props) {
 
   return (
     <View>
-      <Text style={styles.detail}>Detail about {movie.title}</Text>
+      <Text style={styles.detail}>{movie.title}</Text>
+      <View style={styles.starContainer}>
+        <FontAwesomeIcon style={movie.avg_rating > 0 ? styles.orange : styles.white} icon={faStar} />
+        <FontAwesomeIcon style={movie.avg_rating > 1 ? styles.orange : styles.white} icon={faStar} />
+        <FontAwesomeIcon style={movie.avg_rating > 2 ? styles.orange : styles.white} icon={faStar} />
+        <FontAwesomeIcon style={movie.avg_rating > 3 ? styles.orange : styles.white} icon={faStar} />
+        <FontAwesomeIcon style={movie.avg_rating > 4 ? styles.orange : styles.white} icon={faStar} />
+        <Text style={styles.detail}>({movie.no_of_ratings})</Text>
+      </View>
+      <Text style={styles.detail}>{movie.description}</Text>
     </View>
   );
 }
@@ -17,5 +28,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  starContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+  },
+  white: {
+    color: 'black',
+  },
+  orange: {
+    color: 'orange',
   },
 });
